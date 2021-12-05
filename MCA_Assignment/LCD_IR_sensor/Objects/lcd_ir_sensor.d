6f4ae31b449956c9d63192df12b8989183115294
shreeharihm <95577483+shreeharihm@users.noreply.github.com>
2021-12-05 20:04:44 +0530

    Add files via upload

---
+++ MCA_Assignment/LCD_IR_sensor/Objects/lcd_ir_sensor.d
@@ -0,0 +1,2 @@
+.\objects\lcd_ir_sensor.o: LCD_IR_Sensor.c
+.\objects\lcd_ir_sensor.o: C:\Keil_v5\ARM\INC\Philips\lpc214x.h
---
+++ MCA_Assignment/Seven_segment/Objects/7_segment_display.d
@@ -0,0 +1,2 @@
+.\objects\7_segment_display.o: 7_segment_display.c
+.\objects\7_segment_display.o: C:\Keil_v5\ARM\INC\Philips\lpc21xx.h
